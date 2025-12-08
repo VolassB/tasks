43c99052ff1c7249592dc3e775a71530719f8dde
VolassB <boh228pidor@gmail.com>
2025-12-08 20:12:16 +0500

    Completing tasks

---
+++ script.js
@@ -0,0 +1,29 @@
+let title = '';
+if (title != false) {
+    console.log(`Задача: ${title}`)
+}
+else {
+    console.log('Название задачи не указано')
+}
+
+let tasks = 3;
+if (tasks > 3) {
+    console.log('Много задач')
+}
+else if (tasks <= 3) {
+    console.log('Немного задач')
+}
+else if (tasks == 0) {
+    console.log('Список пуст')
+}
+else {
+    console.log('Список не может быть отрицательным, за вами едет ФСБ')
+}
+
+let isComplited = true;
+if (isComplited) {
+    console.log('Задача выполнена')
+}
+else {
+    console.log('Задача ещё в работе')
+}
