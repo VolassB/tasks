bf8ad56a48d37a3a43d77355af0a8454bbce7f5f
VolassB <boh228pidor@gmail.com>
2025-12-08 21:35:54 +0500

    update isComplited

--- script.js
+++ script.js
@@ -21,12 +21,8 @@ else {
 }
 
 let isComplited = true;
-if (isComplited) {
-    console.log('Задача выполнена')
-}
-else {
-    console.log('Задача ещё в работе')
-}
+
+console.log(isComplited ? 'Задача выполнена' : 'Задача ещё в работе');
 
 function sum(first, second) {
     return `${first} + ${second} = ${first + second}`;
@@ -57,13 +53,7 @@ function printTask(title, status) {
 console.log(printTask('Задача', true))
 
 function isTaskDone(status) {
-    if (status === 'выполнена') {
-        return true;
-    }
-
-    else {
-        return false;
-    }
+    return status === 'выполнена';
 }
 
 console.log(isTaskDone('выполнена'));
